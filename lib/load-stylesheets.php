5426cd0656e17b789d9b21ade1acfdf04227f409
Ozzy Rodriguez <ozzy@ozzyrodriguez.com>
2014-01-08 07:17:14 -0800

    updated to 4.0.3

--- lib/load-stylesheets.php
+++ lib/load-stylesheets.php
@@ -4,5 +4,5 @@ add_action( 'wp_enqueue_scripts', 'faif_load_styles' );
 
 // Load FontAwesome stylsheets
 function faif_load_styles() {
-	wp_enqueue_style( 'faif-fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.min.css', array(), '4.0.0' );
+	wp_enqueue_style( 'faif-fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), '4.0.3' );
 }
