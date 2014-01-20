adbafd8eeacc8971924e2d0a75179a574251ab0c
Ozzy Rodriguez <ozzy@ozzyrodriguez.com>
2014-01-20 14:39:09 -0800

    Update setup-shortcodes.php
    updated for current fontawesome naming

--- lib/setup-shortcodes.php
+++ lib/setup-shortcodes.php
@@ -6,7 +6,7 @@ add_filter( 'widget_text', 'do_shortcode' );
 
 function faif_setup_shortcode( $atts ) {
 	extract( shortcode_atts( array(
-		'name' => 'icon-rocket',
+		'name' => 'fa fa-icon-rocket',
 		'size' => '',
 		'pull' => '',
 		'border' => '',
